156ab0cf136ec0ce6b75bacec3515530ddf9f548
Vikas Singh <vikaskuntal10@gmail.com>
2023-03-10 13:31:08 +0530

    Added new Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import CardList from "./CardList";
+import { robots } from './robots.js';
+import SearchBox from './SearchBox.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+    return (
+        <div className="tc">
+            <h1 className="f1">Robo Freak</h1>
+            <SearchBox searchchange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+        </div>
+    )
+    }
 }
 
 export default App;
